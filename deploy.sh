a4d5c167aa1ec85720113d4f42c26073d8da7d59
Lyonzy <14353926+bitlyons@users.noreply.github.com>
2022-11-21 22:33:54 +0000

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-yarn build
-cd dist
-echo 'backup.hachu.ie' > CNAME
-touch .nojekyll
-git init
-git add -A
-git commit -m 'chore: deploy'
-git push -f git@github.com:bitlyons/hachucardbackup.git master:gh-pages
-cd ..
