921cfba3e6aa9fcc58471cada4193806efc87ee2
Lyonzy <14353926+bitlyons@users.noreply.github.com>
2022-11-21 22:34:29 +0000

    Delete src directory

--- src/_shared/components/index.ts
+++
@@ -1,4 +0,0 @@
-import ContentContainer from './ContentContainer';
-import Loader from './Loader';
-
-export { ContentContainer, Loader };
--- src/_shared/utilities/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './card';
-export * from './notification';
-export * from './useWidth';
